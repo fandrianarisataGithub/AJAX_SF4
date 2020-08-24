f0d98ab70cf6bb6fd97534472bc900c864f6b324
Fenitriniaina <git.fandrianarisata@gmail.com>
2020-08-24 12:24:33 +0200

    ajout des inscription admin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,20 +17,13 @@ class SecurityController extends AbstractController
     public function register(Request $request, UserPasswordEncoderInterface $enc )
     {   
         $admin = new Admin();
-        $form_admin = $this->createForm(AdminType::class, $admin);
-
-        // ref mande ny mandeha :D
-        $form_admin->handleRequest($request);
-        // refa soumis le entana
-        if($form_admin->isSubmitted() && $form_admin->isValid()){
-            // on hashe le password
-
-            $hash = $enc->encodePassword($admin, $form->get('password')->getData());
-            dd($admin);
-
-        }
-        return $this->render('security/register.html.twig', [
-            "form_admin" =>$form_admin->createView()
+        $formAdmin= $this->createForm(AdminType::class, $admin, array('method' => 'PUT'));
+        $formAdmin->handleRequest($request);
+        //dd($request);
+        
+        return $this->render('security/register.html.twig', [        
+            "form_admin" =>$formAdmin->createView(),
+            "pass" =>"tsy mbola nisy"
         ]);
     }
 }
